7dfd9ae2c427049c197644e46fa1c831b65e77a1
Victor Calebe <victorcalebesilva10@gmail.com>
2021-08-29 23:51:45 -0300

    (aula-3) Implementando classe ClienteDAO

--- ClinicaVeterinaria/src/main/java/Main.java
+++ ClinicaVeterinaria/src/main/java/Main.java
@@ -1,18 +1,24 @@
 /**
  *
- * @author Victor Calebe
+ * @author victor Calebe
  */
 public class Main {
      public static void main(String[] args) {
         
-        Cliente victor = ClienteDAO.getInstance().retrieveById(2);
+        Cliente calebe = ClienteDAO.getInstance().retrieveById(1);
          
-        //ClienteDAO.getInstance().create("Spiderman","New York City","921456789","00234-900","spider@avengers.com");
-        AnimalDAO.getInstance().create("Bilbo", 2015, "Macho", 1, victor);
+        //ClienteDAO.getInstance().create("calebe","Campinas","123456789","00789-450","calebe@calebe.com");
+        //ClienteDAO.getInstance().create("neymar","Paris","879456123","00000-700","neymar@neymar.com");
+        //AnimalDAO.getInstance().create("Bolis", 2013, "Macho", 7, calebe);
+        
         
         //System.out.println(ClienteDAO.getInstance().retrieveAll());
-        System.err.println(AnimalDAO.getInstance().retrieveAll());
-        //System.out.println(ClienteDAO.getInstance().retrieveById(2));
+        //System.err.println(AnimalDAO.getInstance().retrieveAll());
+        
+        System.out.println(ClienteDAO.getInstance().retrieveById(3));
+        System.out.println(AnimalDAO.getInstance().retrieveById(7));
+        System.out.println(AnimalDAO.getInstance().retrieveById(8));
+        System.out.println(AnimalDAO.getInstance().retrieveById(9));
                 
     }
 }
